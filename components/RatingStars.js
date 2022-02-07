7a90b9dfe2037d5a93afb688508181aa400b663e
Julian Mayes <juliantmayes@gmail.com>
2022-02-07 12:11:18 -0800

    merge fix

---
+++ components/RatingStars.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import { useState } from "react";
+import Checkbox from "./Checkbox";
+import Rating from "@mui/material/Rating";
+
+export default function RatingStars({
+  defaultValue = "4.6",
+  precision = "0.1",
+  readOnly = "true",
+  size = "small",
+  color = "black",
+}) {
+  return (
+    <Rating
+      defaultValue={defaultValue}
+      precision={precision}
+      readOnly={readOnly}
+      size={size}
+      style={{ color: color }}
+    ></Rating>
+  );
+}
--- pages/comps/julian.js
+++ pages/comps/julian.js
@@ -2,16 +2,28 @@ import styled from "styled-components";
 import AddCurriculumForm from "@/components/AddCurriculumForm";
 import FilterDropdown from "@/components/FilterDropdown";
 import FilterDropdownSingle from "@/components/FilterDropdownSingle";
+import RatingStars from "@/components/RatingStars";
+import DifficultyBar from "@/components/DifficultyBar";
 
 export default function julian() {
   return (
     <Cont>
       <AddCurriculumForm />
-      <Space/>
-      <FilterDropdown/>
-      <Space/>
-      <FilterDropdownSingle/>
-      <Space/>
+      <Space />
+      <FilterDropdown />
+      <Space />
+      <FilterDropdownSingle />
+      <Space />
+      <RatingStars
+        defaultValue={4.6}
+        precision={0.5}
+        readOnly={true}
+        size={"small"}
+        color={"black"}
+      />
+      <Space />
+      <DifficultyBar difficulty={"beginner"} size={"small"} />
+      <Space />
     </Cont>
   );
 }
@@ -21,8 +33,8 @@ const Cont = styled.div`
   align-items: center;
   flex-direction: column;
   width: 100%;
-`
+`;
 
 const Space = styled.div`
   height: 100px;
-`
+`;
