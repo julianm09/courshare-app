678df964d0a849591f2d097f0d5de57bbc45c34f
Julian Mayes <juliantmayes@gmail.com>
2022-02-11 13:29:54 -0800

    filtered dataset

--- utils/provider.js
+++ utils/provider.js
@@ -2,7 +2,7 @@ import { useContext, createContext, useState } from "react";
 import { themes } from "./variables";
 //the variables you want to provide to all the pages/components wrapped around this provider
 const initialStates = {
-  theme: "dafault",
+  theme: "default",
   setTheme: () => {},
 };
 
