e76b9aa1f6dde2f05f73fcb0c2d3520630916e7e
834840102 <johnzhou97@gmail.com>
2022-02-14 11:44:31 -0800

    filter update

--- utils/functions/filter.js
+++ utils/functions/filter.js
@@ -2,9 +2,13 @@ export function filtering(
   arr = [],
   config = {
     title: null,
+    university: null,
+    rating: null,
+    level: null,
+    skills: null
   }
 ) {
-  const { title } = config;
+  const { title, university, rating, level, skills } = config;
 
   if (title) {
     const filtered_arr = arr.filter((o) => {
@@ -13,6 +17,23 @@ export function filtering(
       if (title) {
         cond =
           cond && o["Course Name"].toLowerCase().includes(title.toLowerCase());
+      }
+
+      if (university){
+        cond = cond && o.University.includes(university)
+      }
+
+      if(rating){
+        cond = cond && Number(o["Course Rating"]) >= Number(rating);
+      }
+
+      if(level){
+        cond = cond && o["Difficulty Level"].includes(level)
+      }
+
+      if (skills) {
+        cond =
+          cond && o.Skills.toLowerCase().includes(skills.toLowerCase());
       }
 
       return cond;
