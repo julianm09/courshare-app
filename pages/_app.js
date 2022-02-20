6322b96774586d017ae89d9ca50dfea62b2c4246
Julian Mayes <juliantmayes@gmail.com>
2022-02-19 17:23:59 -0800

    v2 before testing

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,33 @@
 import "../styles/globals.css";
 import styled from "styled-components";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
 import Navbar from "@/components/Navbar";
 import AppProvider from "@/utils//provider";
 import "semantic-ui-css/semantic.min.css";
+import { StylesProvider } from "@mui/styles";
 
 function MyApp({ Component, pageProps }) {
+  
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+      
+    }
+
+
+  }, []);
+  
+
   const r = useRouter();
   return (
     <AppProvider>
-      <Navbar />
-      <Component {...pageProps} />
-
-{/*       <BotNav>
-        <button onClick={() => r.push("/")}>Home</button>
-        <button onClick={() => r.push("/comps/evie")}>Evie</button>
-        <button onClick={() => r.push("/comps/john")}>John</button>
-        <button onClick={() => r.push("/comps/juhee")}>Juhee</button>
-        <button onClick={() => r.push("/comps/julian")}>Julian</button>
-      </BotNav> */}
+      <StylesProvider>
+        <Navbar />
+        <Component {...pageProps} />
+      </StylesProvider>
     </AppProvider>
   );
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import Document, { Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components'
-import { ServerStyleSheets } from '@mui/styles';
+import React from "react";
+import Document, { Head, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+import { ServerStyleSheets } from "@mui/styles";
 /* import theme from '../src/theme'; */
 
 class MyDocument extends Document {
   static async getInitialProps (ctx) {
-    const styledComponentsSheet = new ServerStyleSheet()
-    const materialSheets = new ServerStyleSheets()
+    const styledComponentsSheet = await new ServerStyleSheet()
+    const materialSheets = await new ServerStyleSheets()
     const originalRenderPage = ctx.renderPage;
 
     try {
@@ -28,34 +28,6 @@ class MyDocument extends Document {
       } finally {
         styledComponentsSheet.seal()
       }
-  }
-
-  render() {
-    return (
-      <html lang="en" dir="ltr">
-        <Head>
-          <meta charSet="utf-8" />
-          {/* Use minimum-scale=1 to enable GPU rasterization */}
-          <meta
-            name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
-          />
-          {/* PWA primary color */}
-          <meta
-            name="theme-color"
-/*             content={theme.palette.primary.main} */
-          />
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
   }
 }
 
